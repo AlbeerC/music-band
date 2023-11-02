74e1c4d322493caec5bc7bd603c6a5a04e0b976d
Alberto Caminos <albeerc@gmail.com>
2023-11-02 11:32:00 -0300

    new sections

--- main.js
+++ main.js
@@ -5,3 +5,33 @@ const sectionList = document.getElementById("sections-list")
 btnToggle.addEventListener("click", () => {
     sectionList.classList.toggle("none")
 })
+
+
+// Show more
+
+const postList = document.querySelector(".post-list")
+const showMoreButton = document.getElementById("show-more")
+
+let showedPosts = 4
+
+showMoreButton.addEventListener("click", () => {
+    showedPosts += 4
+
+    showPosts(showedPosts)
+})
+
+const showPosts = (quantity) => {
+
+    const posts = document.querySelectorAll(".post")
+
+    posts.forEach((post, index) => {
+        if (index < quantity) {
+            post.style.display = "block";
+        } else {
+            post.style.display = "none";
+        }
+    })
+}
+
+
+showPosts(showedPosts)
